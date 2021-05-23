aed1f28b1c0f9fd2f19a3d4b3b141f71c23a8e22
Eyad <eyadhossamm@gmail.com>
2021-05-23 20:25:47 +0200

    implemented utility function to print enum class as text

--- include/utility.hpp
+++ include/utility.hpp
@@ -1,6 +1,8 @@
 #ifndef UTILITY_HPP
 #define UTILITY_HPP
 
+#include <Token.hpp>
+
 namespace nolang
 {
     [[nodiscard]] auto
@@ -8,6 +10,9 @@ namespace nolang
 
     [[nodiscard]] auto
     IsDigit(char p_byte) noexcept -> bool;
+
+    [[nodiscard]] auto
+    EnumAsString(TokenType p_token) noexcept -> std::string;
 }
 
 #endif
