799de2fbb3da01a00a248c08ae21cff411fc41fc
Eyad <eyadhossamm@gmail.com>
2021-05-23 20:26:55 +0200

    Implemented the ReadWord and ReadInteger methods, and implemented the consumption of multi-character lexemes including identifiers and keywords

--- source/Lexer.cpp
+++ source/Lexer.cpp
@@ -39,6 +39,34 @@ namespace nolang
     }
 
     [[nodiscard]] auto
+    Lexer::ReadWord() noexcept -> std::string
+    {
+        uint32_t start_pos = m_LexerCurrPos;
+        while(IsLetter(m_LexerChar) || IsDigit(m_LexerChar))
+            this->ConsumeChar();
+
+        return m_LexerInput.substr(start_pos, m_LexerCurrPos - start_pos);
+    }
+
+    [[nodiscard]] auto
+    Lexer::ReadInteger() noexcept -> std::string
+    {
+        uint32_t start_pos = m_LexerCurrPos;
+        while(IsDigit(m_LexerChar))
+            this->ConsumeChar();
+
+        return m_LexerInput.substr(start_pos, m_LexerCurrPos - start_pos);
+    }
+
+    [[nodiscard]] auto
+    Lexer::Lookahead() const noexcept -> char
+    {
+        if(m_LexerPeekPos >= m_LexerInput.length())
+            return 0;
+        return m_LexerInput[m_LexerPeekPos];
+    }
+
+    [[nodiscard]] auto
     Lexer::NextToken() noexcept -> nolang::Token
     {
         nolang::Token result_token;
@@ -49,6 +77,60 @@ namespace nolang
 
         switch (lex_char)
         {
+        case '=':
+            if (this->Lookahead() == '=')
+            {
+                this->ConsumeChar();
+                result_token = Token(TokenType::EQ, "==");
+            }
+            else
+                result_token = Token(TokenType::ASSIGN, "=");
+            break;
+        case '!':
+            if (this->Lookahead() == '=')
+            {
+                this->ConsumeChar();
+                result_token = Token(TokenType::NEQ, "!=");
+            }
+            else
+                result_token = Token(TokenType::NOT, "!");
+            break;
+        case '&':
+            if (this->Lookahead() == '&')
+            {
+                this->ConsumeChar();
+                result_token = Token(TokenType::AND, "&&");
+            }
+            else
+                result_token = Token(TokenType::ILLEGAL, "ILLEGAL");
+            break;
+        case '|':
+            if (this->Lookahead() == '|')
+            {
+                this->ConsumeChar();
+                result_token = Token(TokenType::OR, "||");
+            }
+            else
+                result_token = Token(TokenType::ILLEGAL, "ILLEGAL");
+            break;
+        case '<':
+            if (this->Lookahead() == '=')
+            {
+                this->ConsumeChar();
+                result_token = Token(TokenType::LTE, "<=");
+            }
+            else
+                result_token = Token(TokenType::LT, "<");
+            break;
+        case '>':
+            if (this->Lookahead() == '=')
+            {
+                this->ConsumeChar();
+                result_token = Token(TokenType::GTE, ">=");
+            }
+            else
+                result_token = Token(TokenType::GT, ">");
+            break;
         case '+':
             result_token = Token(TokenType::PLUS, "+");
             break;
@@ -80,7 +162,24 @@ namespace nolang
             result_token = Token(TokenType::RBRACE, "}");
             break;
         case 0:
-            result_token = Token(TokenType::EndOfFile, "");
+            result_token = Token(TokenType::EndOfFile, "EOF");
+            break;
+        default:
+            if (IsLetter(lex_char))
+            {
+                auto word = this->ReadWord();
+                auto wordTokenType = nolang::LookupIdentifier(word);
+                result_token = Token(wordTokenType, word);
+                return result_token;
+            }
+            else if (IsDigit(lex_char))
+            {
+                auto word = this->ReadInteger();
+                result_token = Token(TokenType::INT, word);
+                return result_token;
+            }
+            else
+                result_token = Token(TokenType::ILLEGAL, "ILLEGAL");
             break;
         }
 
