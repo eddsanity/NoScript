274db79af690a79462dd385d3761a08017869e24
Eyad <eyadhossamm@gmail.com>
2021-05-23 20:27:40 +0200

    wrote a small nolang program and fed it as input to the lexer to test it in main. Works as expected

--- source/main.cpp
+++ source/main.cpp
@@ -1,16 +1,36 @@
 #include <iostream>
+#include <vector>
+#include <utility.hpp>
 #include <Token.hpp>
 #include <Lexer.hpp>
 
-
 auto main() -> int
 {
-    std::string lexer_input = "{ } ( ) + - / * , ;";
+    std::string lexer_input = "let num1 = 5;" \
+                              "let num2 = 10;" \
+                              "let add = fn(x, y) {" \
+                              "    return x + y;" \
+                              "};" \
+                              " " \
+                              "let result = add(num1, num2);" \
+                              "if(result != num1 && num1 <= num2 || result >= num2) {" \
+                              "    while(result != num1) {" \
+                              "        num1 = num1 + 1;" \
+                              "    }" \
+                              "    return true;" \
+                              "} else {" \
+                              "    return false;" \
+                              "}" \
+                              " " \
+                              "num1 = &num2;" \
+                              "num2 = |num2;";
     nolang::Lexer lexer{lexer_input};
+    std::vector<nolang::Token> tokens;
     
-    for(auto curr_token = lexer.NextToken(); curr_token.m_TokenType != nolang::TokenType::EndOfFile; curr_token = lexer.NextToken())
+    for (auto curr_token = lexer.NextToken(); curr_token.m_TokenType != nolang::TokenType::EndOfFile; curr_token = lexer.NextToken())
     {
-        std::cout << "current token is : " << curr_token.m_TokenLiteral << "\n";
+        // std::cout << "{" << nolang::EnumAsString(curr_token.m_TokenType) << ", \"" << curr_token.m_TokenLiteral << "\"}\n";
+        tokens.push_back(curr_token);
     }
     return 0;
 }
