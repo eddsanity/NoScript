9ce3bb721c987b32b52f58468a01ff8e731fd612
Eyad <eyadhossamm@gmail.com>
2021-05-24 21:06:20 +0200

    Implemented basic Node interface, and the two basic kinds of Nodes, a Statement or an Expression.

---
+++ include/Node.hpp
@@ -0,0 +1,23 @@
+#ifndef NODE_HPP
+#define NODE_HPP
+
+#include <string>
+
+namespace noscript
+{
+    class Node
+    {
+    public:
+        virtual std::string TokenLiteral() = 0;
+    };
+
+    class Statement : public Node
+    {
+    };
+
+    class Expression : public Node
+    {
+    };
+}
+
+#endif
