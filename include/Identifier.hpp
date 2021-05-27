4efcd2cf1b5ce6c84c406cfb9956a34ba729c1f3
Eyad <eyadhossamm@gmail.com>
2021-05-27 22:50:57 +0200

    Constructor for Identifier and ToString implementation

--- include/Identifier.hpp
+++ include/Identifier.hpp
@@ -9,8 +9,10 @@ namespace noscript
     class Identifier : public Expression
     {
     public:
+        Identifier() = default;
+        Identifier(Token p_Token, const std::string& p_TokenLiteral) noexcept : m_Token{p_Token}, m_Value{p_TokenLiteral} {}
         [[nodiscard]] auto TokenLiteral() -> std::string { return m_Token.m_TokenLiteral; }
-
+        [[nodiscard]] auto ToString() -> std::string { return m_Value; }
         Token m_Token;
         std::string m_Value;
     };
