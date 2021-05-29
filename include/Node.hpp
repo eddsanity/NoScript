4a183d4fa4b9d3f4898b5420db02f679e7843816
Eyad <eyadhossamm@gmail.com>
2021-05-29 19:00:46 +0200

    Added virtual destructor for base class Node

--- include/Node.hpp
+++ include/Node.hpp
@@ -11,6 +11,7 @@ namespace noscript
         virtual std::string TokenBody() = 0;
         virtual std::string TokenLiteral() = 0;
         virtual std::string ToString() = 0;
+        virtual ~Node() = default;
     };
 
     class Statement : public Node
